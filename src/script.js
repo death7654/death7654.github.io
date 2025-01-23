fe1e9bc6e03037712478ef221b627fbf44a4762a
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-01-23 23:28:04 +0530

    finish website

--- src/script.js
+++ src/script.js
@@ -4,18 +4,27 @@ const home = document.getElementById('home');
 const menu = document.getElementById('menu');
 const projects = document.getElementById("projects");
 const project_header = document.getElementById("project_header");
+const project_link = document.getElementById("projects_scroll")
 const animation = document.getElementById("typewriter_animation");
+const link_section = document.getElementById("links");
+const body = document.getElementById('body');
+const link_title = document.getElementById("links_title")
 
 
 window.addEventListener('scroll', () => {
   const y = (window.scrollY || window.pageYOffset) / 400
   //console.log(y)
+
+  const y_current = y * 400
+
   const [r, g, b] = [red*y, green*y, blue*y].map(Math.round)
+  if(y_current < 2000)
+  {
   home.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
   projects.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
   menu.style.backgroundColor = `rgb(${r + 20}, ${g + 20}, ${b + 20})`
+  }
 
-  const y_current = y * 400
   if(y_current > 330)
   {
     project_header.classList.remove("display_none");
@@ -23,4 +32,59 @@ window.addEventListener('scroll', () => {
   }
   console.log(y_current)
 })
+
+project_link.addEventListener("click", ()=> {
+  window.scrollTo(0, 945);
+})
+
+function instagram()
+{
+  link_section.classList.add("instagram")
+  body.classList.add("instagram")
+  projects.classList.add("instagram")
+}
+function remove_instagram()
+{
+  link_section.classList.remove("instagram")
+  body.classList.remove("instagram")
+  projects.classList.remove("instagram")
+}
+function github()
+{
+  link_section.classList.add("github")
+  body.classList.add("github")
+  projects.classList.add("github")
+  link_title.classList.add("github_text")
+}
+function remove_github()
+{
+  link_section.classList.remove("github")
+  body.classList.remove("github")
+  projects.classList.remove("github")
+  link_title.classList.remove("github_text")
+}
+function facebook()
+{
+  link_section.classList.add("facebook")
+  body.classList.add("facebook")
+  projects.classList.add("facebook")
+}
+function remove_facebook()
+{
+  link_section.classList.remove("facebook")
+  body.classList.remove("facebook")
+  projects.classList.remove("facebook")
+}
+function linkedin()
+{
+  link_section.classList.add("linkedin")
+  body.classList.add("linkedin")
+  projects.classList.add("linkedin")
+}
+function remove_linkedin()
+{
+  link_section.classList.remove("linkedin")
+  body.classList.remove("linkedin")
+  projects.classList.remove("linkedin")
+}
 
